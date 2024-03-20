62a83c2ce19a1a1053d91e65bc0801cb4a73107c
Victor Zúñiga <gerazgvic@gmail.com>
2024-03-20 10:09:24 -0600

    Columna cantidad removida, por problema de duplicado

--- database/migrations/2024_03_17_150305_create_detalles_salida_almacen_table.php
+++ database/migrations/2024_03_17_150305_create_detalles_salida_almacen_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('detalles_salida_almacen', function (Blueprint $table) {
             $table->id();
             $table->foreignId('detalles_salida_almacen_id')->constrained('salida_almacen')->onDelete('cascade');
-            $table->string('cantidad');
             $table->string('marca');
             $table->string('modelo');
             $table->integer('cantidad');
